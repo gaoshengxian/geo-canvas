b39fc8b33b04e96640f740409590710eaee184a8
ata.aungthawaye <ata.aungthawaye@gmail.com>
2011-05-13 07:48:13 +0000

    1. UI testing

--- Geo Canvas/web/js/ui/app-ui-manager.js
+++ Geo Canvas/web/js/ui/app-ui-manager.js
@@ -2,5 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+function AppUIManager()
+{
+    //this.uiManager = new UIManager(1024,768);
+    //this.uiManager.adjustScreenDimension();
+}
 
-
+AppUIManager.prototype.adjustUI = function()
+{
+};

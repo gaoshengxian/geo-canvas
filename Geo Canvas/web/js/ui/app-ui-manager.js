24bd1dec463e30de4315c56b875f9fa670884b29
ata.aungthawaye <ata.aungthawaye@gmail.com>
2011-05-16 09:32:34 +0000

    1. Added marker table 2. Added javascripts for jGrid 3. Added UI setup method in app ui manager

--- Geo Canvas/web/js/ui/app-ui-manager.js
+++ Geo Canvas/web/js/ui/app-ui-manager.js
@@ -4,10 +4,46 @@
  */
 function AppUIManager()
 {
-    //this.uiManager = new UIManager(1024,768);
-    //this.uiManager.adjustScreenDimension();
+//this.uiManager = new UIManager(1024,768);
+//this.uiManager.adjustScreenDimension();
 }
 
-AppUIManager.prototype.adjustUI = function()
+AppUIManager.prototype.setupUI = function()
 {
+    // Setup Marker Table
+    $("#marker-table").jqGrid(
+    {
+        datatype: "local",
+        height: 100,
+        hidegrid: false,
+        colNames:['Id','Coordinates', 'Title'],
+        colModel:[
+        {
+            name:'id',
+            index:'id',
+            width:60
+        },
+
+        {
+            name:'coordinates',
+            index:'coordinates',
+            width:90
+        },
+
+        {
+            name:'title',
+            index:'title',
+            width:100
+        }],
+        rowNum:15,
+        //rowList:[5,10,15],
+        pager: '#marker-table-nav',
+        multiselect: true,
+        caption: "Information of all the markers on the Map"
+    });
+    $("#marker-table").jqGrid('navGrid','#marker-table-nav',{
+        edit:false,
+        add:false,
+        del:false
+    });
 };
